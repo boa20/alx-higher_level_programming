4ee87d328b8ca8b0f4c3529a0588ba1467f9aefb
boa20 <kwesikyeremateng5@gmail.com>
2022-06-28 22:11:53 +0000

    Display last digit of a random signed number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number:d} is {last_digit:d} and is 0")
 else:
-    print(f"Last digit of {number:d} is {last_digit:d}) 
+    print(f"Last digit of {number:d} is {last_digit:d}") 
     print(" and is less than 6 and not 0")
