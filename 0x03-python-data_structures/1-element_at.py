4229fe989a539c93aa27a0c028f9944cfcc261ec
boa20 <kwesikyeremateng5@gmail.com>
2022-07-04 21:25:12 +0000

    Retrieve element function updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,9 +6,3 @@ def element_at(my_list, idx):
         return None
     else:
         return my_list[idx]
-
-integers = [1, 2, 3, 4, 5]
-print("Element at index {:d} is {}".format(5, element_at(integers, 5)))
-print("Element at index {:d} is {}".format(-2, element_at(integers, -2)))
-print("Element at index {:d} is {}".format(2, element_at(integers, 2)))
-print("Element at index {:d} is {}".format(4, element_at(integers, 4)))
