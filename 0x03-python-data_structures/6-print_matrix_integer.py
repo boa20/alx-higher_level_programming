35f9bc9c1cf5a32eb3a0cfc835cc5ad86906f3c5
boa20 <kwesikyeremateng5@gmail.com>
2022-07-04 22:54:58 +0000

    Display a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
+def print_matrix_integer(matrix=[[]]):
+    # print_matrix_integer - function that prints a matrix of integers
 
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 1:
-            tuple_a = tuple_a[0], 0
-        else:
-            tuple_a = 0, 0
-
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 1:
-            tuple_b = tuple_b[0], 0
-        else:
-            tuple_b = 0, 0
-
-    ans = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return ans
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            if y != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[x][y]), end='')
+        print()
