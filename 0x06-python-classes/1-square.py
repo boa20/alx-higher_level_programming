b734d4dc0ea5d34b47ea04ab636b43bb1d0d39af
boa20 <kwesikyeremateng5@gmail.com>
2022-07-20 04:47:11 +0000

    Class that defines a square and instantiates its size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Defined square class"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
+
