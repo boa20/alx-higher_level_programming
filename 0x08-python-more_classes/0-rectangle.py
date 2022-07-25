cfcb0710f442bd38a39a3db932f8dd37d340b7ea
boa20 <kwesikyeremateng5@gmail.com>
2022-07-25 22:04:52 +0000

    Empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-""" A Rectangle """
+"""A Rectangle"""
 
-class Rectangle:
-    """ An empty Rectangle class """
+class Rectangle():
+    """An empty Rectangle class"""
     pass
