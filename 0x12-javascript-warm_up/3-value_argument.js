9a0832c2752b85a94e51c716956b51287965fcc5
boa20 <kwesikyeremateng5@gmail.com>
2022-10-04 22:11:17 +0000

    Script that displays first argument passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv.length === 3) {
-  console.log(process.argv[2]);
-}
-
-if (process.argv.length > 3) {
-  let i = 2;
-
-  while (i < process.argv.length) {
-    console.log(process.argv[i]);
-    i++;
-  }
-}
+console.log(process.argv[2]);
