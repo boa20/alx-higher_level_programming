71e816bf2533dadf24b9de0554db588df170134c
boa20 <kwesikyeremateng5@gmail.com>
2022-10-07 17:05:29 +0000

    A script that displays three statements

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const statements = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+const len = statements.length;
+
+for (let i = 0; i < len; i++) {
+  console.log(statements[i]);
+}
